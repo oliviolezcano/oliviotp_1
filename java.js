d5f001edc4cdd96de0f154b140ea5af45e9bbdbc
alumno <52211252@ivt.edu.ar>
2026-06-04 10:39:44 -0300

    4 de julio

---
+++ java.js
@@ -0,0 +1,16 @@
+let suma=numero1+numero2
+let resta=numero1-numero2
+let multiplicacion=numero1*numero2
+let division=numwero1/numero2
+let numero1=389
+let numero2=434
+let nombrealumno
+let apellioalumno
+let edadalumno
+let precio1
+let precio2
+let precio3
+let total
+let comidafavorita
+let numerofavorito
+let=paisargentina
